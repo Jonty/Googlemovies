ca04a273c83de1a6e64e1fa81c1ba0ea80ba4d49
Jonty <jonty@jonty.co.uk>
2009-11-12 12:57:29 +0000

    Rewrite parsing to work with the new (Structured!) google movies layout. Vague cleanups etc.

--- googlemovies.pl
+++ googlemovies.pl
@@ -60,31 +60,18 @@ sub parse_html {
     $tree->parse(shift);
     $tree->eof;
 
-    # The table isn't well structured, so we need to switch between
-    # cinemas and movies as we come across them
-    my @rows = $tree->look_down('_tag', 'tr', valign => 'top'); 
+    my @rows = $tree->look_down('_tag', 'div', class => 'theater');
     foreach my $row (@rows) {
-        if (my $cinema = $row->look_down('_tag', 'td', colspan => '4')) {
+        $xml->startTag('Theater');
+        $xml = parse_cinema($xml, $row);
 
-            # If we're in a movies block and we find a new theatre, close
-            $xml->endTag() if $xml->in_element('Movies');
+        my @movierows = $row->look_down('_tag', 'div', class => 'movie');
+        $xml->startTag('Movies');
+        $xml = parse_movies($xml, @movierows);
+        $xml->endTag(); # Movies
 
-            # If we're starting a new Theater block, we need to end the old one
-            $xml->endTag() if $xml->in_element('Theater');
-
-            $xml->startTag('Theater');
-            $xml = parse_cinema($xml, $cinema);
-
-        } elsif (my @movierows = $row->look_down('_tag', 'td', valign => 'top')) {
-
-            # If we're not in a movies block, we need to start one
-            $xml->startTag('Movies') unless $xml->in_element('Movies');
-
-            $xml = parse_movies($xml, @movierows);
-        }
+        $xml->endTag(); # Theater
     }
-    $xml->endTag() if $xml->in_element('Movies');   # Movies
-    $xml->endTag() if $xml->in_element('Theater');  # Theater
 
     if (--$fetch_pages > 0)  {
         my $url = parse_navbar($tree);
@@ -120,51 +107,45 @@ sub parse_navbar {
 sub parse_cinema {
     my ($xml, $cinema) = @_;
 
-    my $name = ($cinema->look_down('_tag', 'b'))[0]->as_text;
-    $name =~ s/&nbsp;/ /g; # Because Myth can't handle the UTF8 representation of &nbsp
-    $name = decode_entities($name);
+    my $name = ($cinema->look_down('_tag', 'h2', class => 'name'))[0]->as_text;
+    $name =~ s/[\xC2\xA0]+//g; # Myth can't handle UTF8 nbsp
     $xml->dataElement('Name', $name);
 
-    my $address = ($cinema->look_down('_tag', 'font'))[0]->as_HTML;
-    $address =~ s/&nbsp;/ /g;
-    $address = decode_entities($address);
-    $address =~ m/>(.*) - [^<]+</;
-    $xml->dataElement('Address', $1);
+    my $address = ($cinema->look_down('_tag', 'div', class => 'info'))[0]->as_text;
+    $address =~ s/[\xC2\xA0]+//g; # Myth can't handle UTF8 nbsp
+    $xml->dataElement('Address', $address);
 
     return $xml;
 }
 
 sub parse_movies {
     my $xml = shift;
-    my @movierows = @_;
 
-    foreach my $movierow (@movierows) {
-        my $movie = ($movierow->look_down('_tag', 'font'))[0]->as_HTML;
+    foreach my $movierow (@_) {
+        $xml->startTag('Movie');
 
-        if ($movie) {
-            $movie =~ s/&nbsp;/ /g;
-            $movie = decode_entities($movie);
-            $xml->startTag('Movie');
+        my $name = ($movierow->look_down('_tag', 'div', class => 'name'))[0]->as_text;
+        $xml->dataElement('Name', $name);
 
-            if ($movie =~ /<b.*?>(.*)<\/b>/i) {
-                $xml->dataElement('Name', $1);
-            }
-
-            if ($movie =~ /Rated\s+(\w+)/i) {
+        my $info = ($movierow->look_down('_tag', 'span', class => 'info'))[0]->as_text;
+        if ($info) {
+            if ($info =~ /Rated\s+(\w+)/i) {
                 $xml->dataElement('Rating', $1);
             }
 
-            if ($movie =~ /<br(?: \/)?>[^\w\s]*([\w\s]+)[^-]*-/i) {
+            if ($info =~ /(\d+hr\s+\d+min)/i) {
                 $xml->dataElement('RunningTime', $1);
             }
-
-            if ((my $showtimes) = ($movie =~ /^.*<br \/>(.*)<\/font>/i)) {
-                $showtimes =~ s/\s+/, /g;
-                $xml->dataElement('ShowTimes', $showtimes);
-            }
-
-            $xml->endTag(); #Movie
         }
+
+        my $showtimes = ($movierow->look_down('_tag', 'div', class => 'times'))[0]->as_text;
+        if ($showtimes) {
+            $showtimes =~ s/[\xC2\xA0]+//g; # Myth can't handle UTF8 nbsp
+            $showtimes =~ s/\s+/, /g;
+            $xml->dataElement('ShowTimes', $showtimes);
+        }
+
+        $xml->endTag(); #Movie
     }
 
     return $xml;
